bc56ffdd39b03e2b45a1deb019131ea49ce701d2
ilko <114412252+iko-ilko@users.noreply.github.com>
2025-12-03 17:42:38 +0900

    fix: 더 확실한 vtt파일 전처리

--- data_pipeline/youtube/processor.py
+++ data_pipeline/youtube/processor.py
@@ -26,18 +26,14 @@ def clean_subtitles(subtitle_text: str) -> str:
     for line in subtitle_text.split('\n'):
         line = line.strip()
 
-        if line.startswith(('Kind:', 'Language:', 'WEBVTT', 'NOTE')):
+        if line.startswith(('Kind:', 'Language:', 'WEBVTT', 'NOTE')) or not line.strip(): # 양쪽 끝에 공백이 없다고 가정(그래보임)하고 제거된 라인 할당 x. 
             continue
 
-        if '-->' in line:
+        if '-->' in line or re.search(r'<\d{2}:\d{2}:', line):
             continue
 
-        if re.search(r'<\d{2}:\d{2}:', line):
-            continue
-
-        if not line.strip(): # 양쪽 끝에 공백이 없다고 가정(그래보임)하고 제거된 라인 할당 x. 
-            continue
-
+        line = line.replace('&gt;', '').replace('[음악]', '')
+        
         # 중복 제거
         if paragraphs and line == paragraphs[-1]:
             continue
